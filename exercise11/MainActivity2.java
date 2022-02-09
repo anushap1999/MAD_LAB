18a31b1971ef9c16311c89a9d323cd4d1ff6bfea
Anusha <74728199+anushap1999@users.noreply.github.com>
2022-02-09 14:42:33 +0530

    Add files via upload

---
+++ exercise11/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(getApplication(),"Welcome",Toast.LENGTH_SHORT).show();
+
+    }
+}
